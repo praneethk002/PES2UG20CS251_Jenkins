ef4dc4242d0eedee31aa5dc7ff33cf12c7ff473f
Praneeth Kumar <praneeth.lakkim@gmail.com>
2023-02-16 09:50:27 +0530

    changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "PES2UG20CS251"
+  cout << "PES2UG20CS251\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
